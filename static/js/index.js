d478c911bb65e685b9a345931ccdb5ce2cd48d4d
Bertik23 <39457484+Bertik23@users.noreply.github.com>
2021-07-25 22:44:58 +0200

    choose what to add + removing

--- static/js/index.js
+++ static/js/index.js
@@ -4,58 +4,51 @@ var tilelayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
   attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
 }).addTo(mymap);
 
-var polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(mymap);
-
-
 function displayObjectOnMap(map, object){
-  console.log(object)
-  fetch(`https://nominatim.openstreetmap.org/search?q=${object}&format=geojson&polygon_geojson=1`)
-    .then(response => {response.json().then(rJson => {
-      var type = rJson["features"][0]["geometry"]["type"]
+  var type = object["geometry"]["type"]
 
-      console.log(rJson)
-      console.log(type)
 
-      switch (type){
-        case "Polygon":
-          var coords = rJson["features"][0]["geometry"]["coordinates"][0]
-          break
-        case "LineString":
-          var coords = rJson["features"][0]["geometry"]["coordinates"]
-          break
-        case "Point":
-          var coords = [rJson["features"][0]["geometry"]["coordinates"]]
-          break
-      }
-      var newCoords = []
-      console.log(coords.length)
-      for (var a=0; a < coords.length; a++){
-        newCoords = newCoords.concat([[coords[a][1], coords[a][0]]])
-      }
-
-      console.log(newCoords)
-      switch (type){
-        case "Polygon":
-          var polygon = L.polygon(newCoords).addTo(map)
-          break
-        case "LineString":
-          console.log(type, "Line")
-          var polygon = L.polyline(newCoords).addTo(map)
-          break
-        case "Point":
-          console.log(type, "Pointík")
-          var polygon = L.marker(newCoords[0]).addTo(map)
-          break
-      }
-      console.log("Added to map")
-      mymap.fitBounds(polygon.getBounds());
-    })
+  switch (type){
+    case "Polygon":
+      var coords = object["geometry"]["coordinates"][0]
+      break
+    case "LineString":
+      var coords = object["geometry"]["coordinates"]
+      break
+    case "Point":
+      var coords = [object["geometry"]["coordinates"]]
+      break
   }
-  )
+  var newCoords = []
+  console.log(coords.length)
+  for (var a=0; a < coords.length; a++){
+    newCoords = newCoords.concat([[coords[a][1], coords[a][0]]])
+  }
+
+  console.log(newCoords)
+  switch (type){
+    case "Polygon":
+      var polygon = L.polygon(newCoords).addTo(map)
+      break
+    case "LineString":
+      console.log(type, "Line")
+      var polygon = L.polyline(newCoords).addTo(map)
+      break
+    case "Point":
+      console.log(type, "Pointík")
+      var polygon = L.marker(newCoords[0]).addTo(map)
+      break
+  }
+  console.log("Added to map")
+  try {mymap.fitBounds(polygon.getBounds());}
+  catch {}
+
+  var addedLayers = document.getElementById("addedLayers")
+  var li = document.createElement("button")
+  li.innerHTML = object["properties"]["display_name"]
+  li.dataset.id = polygon._leaflet_id
+  li.onclick = function(){removeLayerId(mymap, this.dataset.id); this.remove()}
+  addedLayers.appendChild(li)
 }
 
 function toggleTileLayer(map, overwrite=null){
@@ -69,4 +62,33 @@ function toggleTileLayer(map, overwrite=null){
   } else {
     tilelayer.addTo(map)
   }
+}
+
+/**
+ * Searches for objects and return list of features
+ * @param {String} object
+ * @returns {JSON}
+ */
+async function searchObject(object){
+  var rJson = await (await fetch(`https://nominatim.openstreetmap.org/search?q=${object}&format=geojson&polygon_geojson=1`)).json()
+  return rJson["features"]
+}
+
+function searchFor(){
+  var quary = document.getElementById("addToMap").value;
+  var resultList = document.getElementById("searchResults")
+  searchObject(quary).then(result => {
+    for (var feature of result){
+      var li = document.createElement("button")
+      li.innerHTML = feature["properties"]["display_name"]
+      li.dataset.feature = JSON.stringify(feature)
+      li.onclick = function() {resultList.style.display = "none"; displayObjectOnMap(mymap, JSON.parse(this.dataset.feature))}
+      resultList.appendChild(li)
+    }
+    resultList.style.removeProperty("display")
+  })
+}
+
+function removeLayerId(map, id){
+  map.eachLayer(function(layer){if (layer._leaflet_id == id){layer.removeFrom(map)}})
 }
