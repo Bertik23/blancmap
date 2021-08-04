6b28408b5b76065594b0fb17e7ecba0d6573a6d0
Bertik23 <39457484+Bertik23@users.noreply.github.com>
2021-08-04 20:22:14 +0200

    PEP-8

--- forms.py
+++ forms.py
@@ -1,11 +1,31 @@
 from wtforms import Form, TextField, PasswordField, validators
 from wtforms.fields.html5 import EmailField
 
+
 class RegistrationForm(Form):
-    username = TextField("Username", [validators.Length(max=255)], render_kw={"class": "form-control"})
-    email = EmailField('Email', [validators.Email()], render_kw={"class": "form-control"})
-    password = PasswordField("Password", render_kw={"class": "form-control"})
+    username = TextField(
+        "Username",
+        [validators.Length(max=255)],
+        render_kw={"class": "form-control"}
+    )
+    email = EmailField(
+        'Email',
+        [validators.Email()],
+        render_kw={"class": "form-control"}
+    )
+    password = PasswordField(
+        "Password",
+        render_kw={"class": "form-control"}
+    )
+
 
 class LoginForm(Form):
-    username = TextField("Username", [validators.Length(max=255)], render_kw={"class": "form-control"})
-    password = PasswordField("Password", render_kw={"class": "form-control"})
+    username = TextField(
+        "Username",
+        [validators.Length(max=255)],
+        render_kw={"class": "form-control"}
+    )
+    password = PasswordField(
+        "Password",
+        render_kw={"class": "form-control"}
+    )
